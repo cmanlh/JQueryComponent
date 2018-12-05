69a90772b699b95994290baa6e8a382d3bc3e574
mawenjie <mawenjie@orientsec.com.cn>
2018-12-05 14:40:38 +0800

    更新calendar组件

--- com/lifeonwalden/jqc/calendar/test/test.js
+++ com/lifeonwalden/jqc/calendar/test/test.js
@@ -8,76 +8,90 @@ $JqcLoader.importScript('../../../../../qunit/jquery-3.1.1.js')
             .registerComponent('select')
             .registerComponent('valHooks')
             .registerComponent('uniqueKey')
+            .registerComponent('toolkit')
+            .registerComponent('contextmenu')
+            .registerComponent('timepicker')
+            .registerComponent('datetimepicker')
+            .registerComponent('notification')
+            .registerComponent('zindex')
+            .registerComponent('dateUtil')
+            .registerComponent('tip')
             .registerComponent('calendar'));
 
         $JqcLoader.importComponents('com.lifeonwalden.jqc', ['calendar']).execute(function () {
-            var A_data = [];
-            window.a = new $.jqcCalendar({
-                title: function (year, month) {
-                    return `${year}年${month}月 - 默认`
-                },
-                el: $('.calendar'), 
-                cellHeight: 100,
+            var A_data = [{
+                type: 1,
+                time: '2018-11-26',
+                content: '测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试',
+                tip: '11:23'
+            }, {
+                time: '2018-11-28',
+                content: '测试测试测试测试'
+            }, {
+                time: '2018-11-29',
+                content: '测试测试测试测试'
+            }, {
+                time: '2018-11-30',
+                content: '测试测试测试测试',
+                id: 100,
+                tip: '12:25'
+            }, {
+                time: '2018-12-01',
+                content: '测试测试测试测试'
+            }, {
+                time: '2018-12-02',
+                content: '测试测试测试测试'
+            }];
+            var calendar = new $.jqcCalendar({
+                title: '银行间节假日',
+                el: $('.calendar'),
                 startYear: 2018,
-                endYear: 2019,
-                // cellTextAlign: 'left',
+                endYear: 2020,
                 data: A_data,
-                adapter: 'time',
-                cellRender: function (data, cell, fillData) {
-                    var config = {};
-                    if (data.day == 6 || data.day == 7) {
-                        config.type = 'success';
-                    } else {
-                        config.type = 'error'
-                    }
-                    if (fillData) {
-                        config.type = 'warn';
-                        config.content = fillData.content;
-                    }
-                    return config;
-                },
-                onSelect: function (data) {
-                    console.log(data);
-                },
-                onSave: function (data) {
-                    console.log(data);
-                }
-            });
-
-            window.b = new $.jqcCalendar({
-                title: function (year, month) {
-                    return `${year}年${month}月 - 可编辑`
-                },
-                el: $('.calendar2'), 
-                cellHeight: 100,
-                startYear: 2018,
-                endYear: 2019,
-                adapter: function (rowData) {
-                    return rowData.time;
-                },
-                data: [{
-                    time: '2019-01-01',
-                    content: '123456'
-                }],
+                cellHeight: 130,
                 canEditor: true,
-                cellRender: function (data, cell, fillData) {
-                    var config = {};
-                    if (data.day == 6 || data.day == 7) {
-                        config.type = 'success';
-                    } else {
-                        config.type = 'error'
+                adapter: {
+                    startTime: function (data) {
+                        return +new Date(data.time);
+                    },
+                    endTime: function (data) {
+                        return +new Date(data.time);
+                    },
+                    type: function (data) {
+                        return data.type || 4
+                    },
+                    tip: function (data) {
+                        return data.tip
                     }
-                    if (fillData) {
-                        config.type = 'warn';
-                        config.content = fillData.content;
-                    }
-                    return config;
                 },
-                mode: 'editor',
-                onSave: function (data) {
-                    A_data = data;
-                    console.log(data);
-                    a.reRender(data);
+                cellRender: function (data, cell, memos) {
+                    var x = [];
+                    if (data.day == 6 || data.day == 7) {
+                        cell.addClass('error');
+                        var config1 = {};
+                        config1.type = '2';
+                        config1.content = '周末';
+                        x.push({data: config1});
+                    }
+                    return x.concat(memos);
+                },
+                onSelect: function (data, memos) {
+                    console.log(data, memos);
+                },
+                onMemoSelect: function (memo) {
+                    console.log(memo);
+                },
+                onMemoChange: function (memo) {
+                    console.log('change', memo);
+                },
+                onMemoDelete: function (memo) {
+                    console.log('delete',memo);
+                },
+                onMemoAdd: function (memo) {
+                    console.log('add',memo);
+                },
+                onMemoUpdate: function (memo) {
+                    console.log('update',memo);
                 }
             });
         });
