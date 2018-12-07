a0574ea63b144695ec7f0258736a783399155102
mawenjie <mawenjie@orientsec.com.cn>
2018-12-07 09:32:31 +0800

    添加icon组件

---
+++ com/lifeonwalden/jqc/icon/test/test.js
@@ -0,0 +1,12 @@
+$JqcLoader.importScript('../../../../../qunit/jquery-3.1.1.js')
+    .importScript('../../../../../qunit/version.js')
+    .importScript('../../../../../qunit/keycode.js')
+    .importScript('../../../../../qunit/qunit-2.1.1.js')
+    .importCss('../../../../../qunit/qunit-2.1.1.css').execute(function () {
+        $JqcLoader.registerModule($JqcLoader.newModule('com.lifeonwalden.jqc', '../../../../../')
+            .registerComponent('icon'));
+
+        $JqcLoader.importComponents('com.lifeonwalden.jqc', ['icon']).execute(function () {
+
+        });
+    });
