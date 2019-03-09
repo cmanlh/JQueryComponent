f86867f5bb319928e7b5aaee6f65010cc6bb45d6
luhong <cmanlh@163.com>
2019-03-09 17:57:48 +0800

    add aes-gcm decrypt

--- com/lifeonwalden/jqc/crypto/crypto.js
+++ com/lifeonwalden/jqc/crypto/crypto.js
@@ -20,6 +20,23 @@
     $JqcLoader.importComponents('com.lifeonwalden.jqc', ['charUtil'])
         .execute(function () {
             var subtle = window.crypto.subtle;
+            function ivPreprocess(iv) {
+                let _iv = iv;
+                if (null == _iv || (null == _iv.byteBuffer && null == _iv.text)) {
+                    _iv = {};
+                    _iv.byteBuffer = window.crypto.getRandomValues(new Uint8Array(12));
+                    _iv.text = $.charUtil.byteToU64(_iv.byteBuffer);
+                } else {
+                    if (null == _iv.byteBuffer && null != _iv.text) {
+                        _iv.byteBuffer = new Uint8Array($.charUtil.u64ToByte(_iv.text));
+                    } else if (null != _iv.byteBuffer && null == _iv.text) {
+                        _iv.text = $.charUtil.byteToU64(_iv.byteBuffer);
+                    }
+                }
+
+                return _iv;
+            }
+
             $.crypto = {
                 aesGenerateKeyAsync: function (name = 'AES-GCM', size = 256, usage = ['encrypt', 'decrypt']) {
                     return new Promise((resolve, reject) => {
@@ -28,15 +45,42 @@
                             length: size
                         }, true, usage).then(key => {
                             subtle.exportKey('raw', key).then(exportKey => {
-                                resolve($.charUtil.encodeArrayBuffer(exportKey));
+                                resolve($.charUtil.byteToU64(exportKey));
                             });
                         }).catch(err => reject(err));
                     });
                 },
                 importKeyAsync: function (key, format = 'raw', name = 'AES-GCM', usage = ['encrypt', 'decrypt']) {
                     return new Promise((resolve, reject) => {
-                        subtle.importKey(format, $.charUtil.decodeArrayBuffer(key), name, true, usage)
+                        subtle.importKey(format, $.charUtil.u64ToByte(key), name, true, usage)
                             .then(importedKey => resolve(importedKey))
+                            .catch(err => reject(err));
+                    });
+                },
+                encryptAesGcmAsync: function (key, text, iv) {
+                    let _iv = ivPreprocess(iv);
+                    let algorithm = {
+                        name: 'AES-GCM',
+                        iv: _iv.byteBuffer
+                    };
+                    return new Promise((resolve, reject) => {
+                        subtle.encrypt(algorithm, key, new Uint8Array($.charUtil.textToByte(text)))
+                            .then(encrypted => resolve({
+                                text: $.charUtil.byteToU64(encrypted),
+                                iv: _iv
+                            }))
+                            .catch(err => reject(err));
+                    });
+                },
+                decryptAesGcmAsync: function (key, text, iv) {
+                    let _iv = ivPreprocess(iv);
+                    let algorithm = {
+                        name: 'AES-GCM',
+                        iv: _iv.byteBuffer
+                    };
+                    return new Promise((resolve, reject) => {
+                        subtle.decrypt(algorithm, key, new Uint8Array($.charUtil.u64ToByte(text)))
+                            .then(decrypted => resolve($.charUtil.byteToText(new Uint8Array(decrypted))))
                             .catch(err => reject(err));
                     });
                 }
--- com/lifeonwalden/jqc/crypto/test/test.js
+++ com/lifeonwalden/jqc/crypto/test/test.js
@@ -5,7 +5,7 @@ $JqcLoader.importScript('../../../../../qunit/jquery-3.1.1.js')
             .registerComponents(['charUtil', 'crypto']));
 
         $JqcLoader.importComponents('com.lifeonwalden.jqc', ['crypto']).execute(function () {
-            QUnit.test("String", function (assert) {
+            QUnit.test("key", function (assert) {
                 assert.expect(2);
                 let done = assert.async(2);
                 $.crypto.aesGenerateKeyAsync()
@@ -16,9 +16,35 @@ $JqcLoader.importScript('../../../../../qunit/jquery-3.1.1.js')
                         $.crypto.importKeyAsync(key)
                             .then(importedKey => {
                                 crypto.subtle.exportKey('raw', importedKey).then(exportedKey => {
-                                    assert.equal($.charUtil.encodeArrayBuffer(exportedKey), key, $.charUtil.encodeArrayBuffer(exportedKey));
+                                    assert.equal($.charUtil.byteToU64(exportedKey), key, $.charUtil.byteToU64(exportedKey));
                                     done();
                                 });
+                            });
+                    });
+            });
+            QUnit.test("encrypt", function (assert) {
+                let text = "AES-GCM加密测试";
+                let done = assert.async(2);
+                $.crypto.aesGenerateKeyAsync()
+                    .then(key => {
+                        $.crypto.importKeyAsync(key)
+                            .then(importedKey => {
+                                $.crypto.encryptAesGcmAsync(importedKey, text)
+                                    .then(result => {
+                                        assert.equal(1, 1, result.text);
+                                        done();
+
+                                        $.crypto.decryptAesGcmAsync(importedKey, result.text, result.iv)
+                                            .then(decryptedText => {
+                                                assert.equal(decryptedText, text, decryptedText);
+                                                done();
+                                            })
+                                            .catch(err => {
+                                                assert.equal(1, 0, err);
+                                                console.error(err);
+                                                done();
+                                            });
+                                    });
                             });
                     });
             });
